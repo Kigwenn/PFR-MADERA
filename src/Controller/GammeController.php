7f9239df244da5ee7e70d664ebc2c84074c59d3b
aurelien <aurelien.place@viacesi.fr>
2019-11-23 20:00:30 +0100

    liste modules (gamme en paramètre)

--- src/Controller/GammeController.php
+++ src/Controller/GammeController.php
@@ -51,4 +51,62 @@ class GammeController extends AbstractController
         $reponse->headers->set("Access-Control-Allow-Origin", "*"); 
         return $reponse;
     }
+
+    /**
+    * Permet d'avoir la liste de tous les modules 
+    * @Route("/modules/", name="gamme_modules", methods={"GET"});
+    */
+    public function listeModules(Request $requestjson) 
+    {
+        $parametersAsArray = [];
+        $erreur = null;
+        //Conversion dU JSON
+        if ($content = $requestjson->getContent()) {
+            $parametersAsArray = json_decode($content, true);
+        }
+        //Verification parametres
+        if ($parametersAsArray == null){
+            $erreur = "Il n'y a pas de paramètre.";
+        }else{
+            $entityManager = $this->getDoctrine()->getManager(); 
+            $repository_gamme = $this->getDoctrine()->getRepository(Gamme::class); 
+            $gamme = $repository_gamme->find($parametersAsArray['gamme_module_id']);
+            if ($gamme == null) {
+                $erreur = "La gamme n'existe pas.";
+            }else{
+                //Recupérattion des modules
+                $listeModules = $gamme->getModules();
+                //Verification de la base
+                if ($listeModules == null) {
+                    $erreur = "Aucun module trouvé.";
+                }else{
+                    $listeReponse = array();
+                    foreach ($listeModules as $module) 
+                    {
+                        $listeReponse[] = array(
+                            'id' => $module->getId(),
+                            'nom_module' => $module->getNomModule(),
+                        );
+                    }
+                }
+            }
+        }
+        //Envoi de la réponse 
+        if  ($erreur == null) { 
+            $reponse = new Response (json_encode(array(
+                'result' => "OK",
+                'gamme_module_id' => $parametersAsArray['gamme_module_id'],
+                "listeModules" => $listeReponse,
+                )
+            ));
+        } else {
+            $reponse = new Response (json_encode(array(
+                'result' => $erreur,
+                )
+            ));
+        }
+        $reponse->headers->set("Content-Type", "application/json"); 
+        $reponse->headers->set("Access-Control-Allow-Origin", "*"); 
+        return $reponse;
+    }
 }
