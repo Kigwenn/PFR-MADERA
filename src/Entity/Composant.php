0e5d44db1f878bc76afaedb9d18a2dd0c82e89b2
aurelien <aurelien.place@viacesi.fr>
2019-12-23 22:28:05 +0100

    correction base

--- src/Entity/Composant.php
+++ src/Entity/Composant.php
@@ -24,11 +24,6 @@ class Composant
     private $comp_nom;
 
     /**
-     * @ORM\Column(type="float")
-     */
-    private $comp_prix;
-
-    /**
      * @ORM\Column(type="string", length=100)
      */
     private $comp_type;
@@ -51,7 +46,7 @@ class Composant
     /**
      * @ORM\Column(type="decimal", precision=6, scale=2)
      */
-    private $comp_unite_usage;
+    private $comp_prix_unitaire;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\ComposantModule")
@@ -88,18 +83,6 @@ class Composant
         return $this;
     }
 
-    public function getCompPrix(): ?float
-    {
-        return $this->comp_prix;
-    }
-
-    public function setCompPrix(float $comp_prix): self
-    {
-        $this->comp_prix = $comp_prix;
-
-        return $this;
-    }
-
     public function getCompType(): ?string
     {
         return $this->comp_type;
@@ -112,43 +95,17 @@ class Composant
         return $this;
     }
 
-    public function getCompUniteUsage(): ?string
+    public function getCompPrixUnitaire(): ?string
     {
-        return $this->comp_unite_usage;
+        return $this->comp_prix_unitaire;
     }
 
-    public function setCompUniteUsage(string $comp_unite_usage): self
+    public function setCompPrixUnitaire(string $comp_prix_unitaire): self
     {
-        $this->comp_unite_usage = $comp_unite_usage;
+        $this->comp_prix_unitaire = $comp_prix_unitaire;
 
         return $this;
     }
-
-    /**
-     * @return Collection|Fournisseur[]
-     */
-    // public function getFournisseurs(): Collection
-    // {
-    //     return $this->fournisseurs;
-    // }
-
-    // public function addFournisseur(Fournisseur $fournisseur): self
-    // {
-    //     if (!$this->fournisseurs->contains($fournisseur)) {
-    //         $this->fournisseurs[] = $fournisseur;
-    //     }
-
-    //     return $this;
-    // }
-
-    // public function removeFournisseur(Fournisseur $fournisseur): self
-    // {
-    //     if ($this->fournisseurs->contains($fournisseur)) {
-    //         $this->fournisseurs->removeElement($fournisseur);
-    //     }
-
-    //     return $this;
-    // }
 
     /**
      * @return Collection|Stock[]
@@ -176,32 +133,6 @@ class Composant
         return $this;
     }
 
-    /**
-     * @return Collection|Caracteristique[]
-     */
-    public function getCaracteristiquesComposant(): Collection
-    {
-        return $this->caracteristiquesComposant;
-    }
-
-    public function addCaracteristiquesComposant(Caracteristique $caracteristiquesComposant): self
-    {
-        if (!$this->caracteristiquesComposant->contains($caracteristiquesComposant)) {
-            $this->caracteristiquesComposant[] = $caracteristiquesComposant;
-        }
-
-        return $this;
-    }
-
-    public function removeCaracteristiquesComposant(Caracteristique $caracteristiquesComposant): self
-    {
-        if ($this->caracteristiquesComposant->contains($caracteristiquesComposant)) {
-            $this->caracteristiquesComposant->removeElement($caracteristiquesComposant);
-        }
-
-        return $this;
-    }
-
     public function getFami(): ?FamilleComposant
     {
         return $this->fami;
@@ -212,47 +143,5 @@ class Composant
         $this->fami = $fami;
 
         return $this;
-    }
-
-    // public function getModules(): ?ComposantModule
-    // {
-    //     return $this->modules;
-    // }
-
-    // public function setModules(?ComposantModule $modules): self
-    // {
-    //     $this->modules = $modules;
-
-    //     return $this;
-    // }
-
-    /**
-     * @return Collection|ComposantModule[]
-     */
-    // public function getComposantModules(): Collection
-    // {
-    //     return $this->composantModules;
-    // }
-
-    // public function addComposantModule(ComposantModule $composantModule): self
-    // {
-    //     if (!$this->composantModules->contains($composantModule)) {
-    //         $this->composantModules[] = $composantModule;
-    //         $composantModule->addComp($this);
-    //     }
-
-    //     return $this;
-    // }
-
-    // public function removeComposantModule(ComposantModule $composantModule): self
-    // {
-    //     if ($this->composantModules->contains($composantModule)) {
-    //         $this->composantModules->removeElement($composantModule);
-    //         $composantModule->removeComp($this);
-    //     }
-
-    //     return $this;
-    // }
-
-    
+    }   
 }
---
+++ src/Migrations/Version20191223212656.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191223212656 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE composant DROP comp_prix, CHANGE comp_unite_usage comp_prix_unitaire NUMERIC(6, 2) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE composant ADD comp_prix DOUBLE PRECISION NOT NULL, CHANGE comp_prix_unitaire comp_unite_usage NUMERIC(6, 2) NOT NULL');
+    }
+}
